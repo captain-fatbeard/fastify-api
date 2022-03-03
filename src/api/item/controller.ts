7dc9785bda1cb054ab01333d75ae3bffde593dbc
jacob juul larsen <jjl@ambition.dk>
2022-03-03 13:37:43 +0100

    added prisma

--- src/api/item/controller.ts
+++ src/api/item/controller.ts
@@ -1,17 +1,30 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 export const indexItems = async (
     req: FastifyRequest,
     res: FastifyReply,
 ): Promise<FastifyReply> => {
-    return res.send({ data: {} });
+    const allItems = await prisma.item.findMany();
+    console.log(allItems);
+
+    return res.send({ data: allItems });
 };
 
 export const createItem = async (
     req: FastifyRequest,
     res: FastifyReply,
 ): Promise<FastifyReply> => {
-    return res.code(201).send({ data: {} });
+    const newItem = await prisma.item.create({
+        data: {
+            name: 'Alice',
+        },
+    });
+
+    return res.code(201).send({ data: newItem });
 };
 
 module.exports.autoload = false;
