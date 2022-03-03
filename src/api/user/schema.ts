fbb12bd805e0181de422bc44a214a7897732adae
Jacob Juul Larsen <jjl@ambition.dk>
2022-03-03 23:23:48 +0100

    started building response schema

--- src/api/user/schema.ts
+++ src/api/user/schema.ts
@@ -1,11 +1,23 @@
 import { Type, Static } from '@sinclair/typebox';
 
-const createUserSchema = Type.Object({
+const userDefaults = {
     email: Type.String({
         format: 'email',
     }),
     name: Type.String(),
+    phone: Type.String(),
+    role: Type.Number(),
+    validated: Type.String(),
+};
+
+const createUserSchema = Type.Object({
+    ...userDefaults,
     password: Type.String(),
 });
+
+// const createUserResponseSchema = Type.Object({
+//     id: Type.Number(),
+//     ...userDefaults,
+// });
 
 export type CreateUserInput = Static<typeof createUserSchema>;
